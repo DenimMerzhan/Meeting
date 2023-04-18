e445ff004ae7009125fcb408bb99ee740b355299
Denim <dagn456@mail.ru>
2023-04-18 10:27:29 +0400

    Initial Commit

---
+++ Meeting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meeting
+//
+//  Created by Деним Мержан on 16.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
