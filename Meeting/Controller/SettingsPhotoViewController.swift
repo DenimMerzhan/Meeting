40f1c79db7ad94d6052b7cdeabf54475258b7504
Denim <dagn456@mail.ru>
2023-05-01 10:53:37 +0400

    Добавлены кнопки удаления и добавления фото в CollageView

--- Meeting/Controller/SettingsPhotoViewController.swift
+++ Meeting/Controller/SettingsPhotoViewController.swift
@@ -27,42 +27,72 @@ class SettingsPhotoViewController: UIViewController {
 }
 
 
+
+
+//MARK:  - Настройка фотоколлажа
+
 extension SettingsPhotoViewController : UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 9
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageUserCell", for: indexPath)
         cell.layer.cornerRadius = 10
         cell.layer.masksToBounds = true
-        
+      
         
         if let imageView = createImage(indexPath: indexPath.row, cellWidth: cell.frame.width, cellHeight: cell.frame.height) {
+            
             cell.contentView.addSubview(imageView)
+            let button = createButton(x: cell.frame.maxX ,y: cell.frame.maxY,add: false)
+            collectionPhotoView.addSubview(button)
+            
         }else {
             cell.backgroundColor = UIColor(named: "PhotoCollage")
+            
             let newBorder = createDottedLine(bounds: cell.bounds)
             cell.layer.addSublayer(newBorder)
+            
+            let button = createButton(x: cell.frame.maxX ,y: cell.frame.maxY,add: true)
+            collectionPhotoView.addSubview(button)
             
         }
         
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (collectionView.frame.size.width / 3) - 8 , height: (collectionView.frame.height / 3) - 8)
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize { /// Расчитываем размеры ячейки
+        return CGSize(width: (collectionView.frame.size.width / 3) - 14 , height: (collectionView.frame.height / 3) - 20)
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat { /// Запрашивает у делегата расстояние между последовательными строками или столбцами раздела.
+        return 15
+    }
     
     
-    func createImage(indexPath: Int,cellWidth: CGFloat,cellHeight: CGFloat) -> UIImageView? {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets { /// Делаем отступы
+        return UIEdgeInsets(top: 0, left: 3, bottom: 0, right: 10)
+    }
+    
+    
+}
+
+
+//MARK: - Внешние оформление ячеек
+
+
+extension SettingsPhotoViewController {
+        
+    func createImage(indexPath: Int,cellWidth: CGFloat,cellHeight: CGFloat) -> UIImageView? { /// Создание фото
         
         if indexPath < imageArr.count {
-            print(indexPath)
             let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: cellWidth, height: cellHeight))
             imageView.image = imageArr[indexPath]
             imageView.contentMode = .scaleAspectFill
@@ -86,5 +116,39 @@ extension SettingsPhotoViewController : UICollectionViewDataSource, UICollection
         
         return viewBorder
         
+    }
+    
+    
+    
+//MARK: - Создание кнопок удаления и добавления
+    
+    
+    func createButton(x: CGFloat, y: CGFloat, add: Bool) -> UIButton {
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0,width: 30, height: 30))
+        button.center = CGPoint(x: x - 5, y: y - 5)
+        button.layer.cornerRadius = button.frame.size.width / 2
+        button.layer.masksToBounds = true
+        
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = .zero
+        button.layer.opacity = 1
+        button.layer.shadowRadius = 10
+        
+        if add {
+            
+            button.backgroundColor = UIColor(named: "MainAppColor")
+            button.setImage(UIImage(named: "Plus"), for: .normal)
+            button.tintColor = UIColor.white
+            return button
+        }else {
+            button.backgroundColor = .white
+            button.setImage(UIImage(named: "DeletePhoto1"), for: .normal)
+            button.tintColor = UIColor.gray
+            button.layer.borderWidth = 0.5
+            button.layer.borderColor = UIColor.gray.cgColor
+
+            return button
+        }
     }
 }
