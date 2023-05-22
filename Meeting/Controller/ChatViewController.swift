8cb33e4214f389cdbe1a87b0f1e56e7c110a6dea
Denim <dagn456@mail.ru>
2023-05-22 17:46:06 +0400

    ChatViewCell настроен

--- Meeting/Controller/ChatViewController.swift
+++ Meeting/Controller/ChatViewController.swift
@@ -9,14 +9,28 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let cell = createChatViewCell()
+        view.addSubview(cell)
         
-        tableView.register(UINib(nibName: "ChatCell", bundle: nil), forCellReuseIdentifier: "chatCell")
     }
     
 
+}
+
+extension ChatViewController {
+    func createChatViewCell() -> ChatCellView {
+        
+        let chatCell = ChatCellView(x: 10, y: 200, width: view.frame.width)
+        
+        chatCell.avatar.image = UIImage(named: "KatyaS")
+        chatCell.nameLabel.text = "Алиса"
+        chatCell.commentLabel.text = "Привет как дела, все хорошо?"
+                                    
+      
+        return chatCell
+    }
 }
--- Meeting/CustomClass/ChatViewCell.swift
+++ Meeting/CustomClass/ChatViewCell.swift
@@ -6,30 +6,50 @@ class ChatCellView: UIView {
     let avatar = UIImageView(frame: CGRect(x: 10, y: 10, width: 80, height: 80))
     let nameLabel =  UILabel()
     let commentLabel =  UILabel()
+    private let bottomLine = UIView()
     
-    init(frame: CGRect,name: String,comment:String,avatar: UIImage) {
-        
-        self.nameLabel.text = name
-        self.commentLabel.text = comment
-        self.avatar.image = avatar
-        super.init(frame: frame)
-        
+    init(x: CGFloat,y: CGFloat,width: CGFloat) {
+        super.init(frame: CGRect(x: x, y: y, width: width, height: 100))
     }
     
     override func layoutSubviews() {
+        
         super.layoutSubviews()
-        avatar.frame.size.height = self.frame.height - 20
-        avatar.frame.size.width = self.frame.height - 20
-        nameLabel.frame = CGRect(x: avatar.frame.maxX + 5, y: self.center.y - 50, width: 200, height: 50)
-        commentLabel.frame = CGRect(x: avatar.frame.maxX + 5, y: self.center.y + 50, width: self.frame.width, height: 50)
+        
+        avatar.frame.size.height = 80
+        avatar.frame.size.width = 80
+        
+        nameLabel.frame = CGRect(x: avatar.frame.maxX + 5, y: 25, width: 200, height: 30)
+        
+        commentLabel.frame = CGRect(x: avatar.frame.maxX + 5, y: 60, width: self.frame.width, height: 20)
+        
+        avatar.contentMode = .scaleAspectFill
+        avatar.layer.cornerRadius = avatar.frame.width / 2
+        avatar.clipsToBounds = true
+        
+        nameLabel.font = .boldSystemFont(ofSize: 30)
+        commentLabel.font = .systemFont(ofSize: 12)
+        commentLabel.textColor = .gray
+        
+        
+        bottomLine.frame = CGRect(x: avatar.frame.maxX, y: 99, width: self.frame.width, height: 1)
+        bottomLine.backgroundColor = .gray
+        bottomLine.layer.cornerRadius = 10
+        bottomLine.clipsToBounds = true
+        bottomLine.alpha = 0.3
+        
+        
+        
+        self.backgroundColor = .clear
+        
+        
+        
+        self.addSubview(avatar)
         self.addSubview(nameLabel)
         self.addSubview(commentLabel)
-        self.addSubview(avatar)
-        print("Yeah")
-        
+        self.addSubview(bottomLine)
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
     
 }
