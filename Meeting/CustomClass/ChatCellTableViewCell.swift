f6e964f2a61a0c66526a93b8d51dafeca4d4d595
Denim <dagn456@mail.ru>
2023-05-22 11:36:02 +0400

    Создана пользовательская ячейка чата

---
+++ Meeting/CustomClass/ChatCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ChatCellTableViewCell.swift
+//  Meeting
+//
+//  Created by Деним Мержан on 22.05.23.
+//
+
+import UIKit
+
+class ChatCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var avatarUser: UIImageView!
+    @IBOutlet weak var nameUser: UILabel!
+    @IBOutlet weak var comment: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        avatarUser.layer.cornerRadius = 10
+        avatarUser.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
